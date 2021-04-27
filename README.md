9126878fe13249a3b422e0214c456c4666cf1ede
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-27 10:54:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cats_app_test_task
+
+Flutter Cat Test Application
+
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cats app'),
+      ),
+      body: Center(child: const Text('Bite my shiny metal ass!!!!')),
+    );
+  }
+}
