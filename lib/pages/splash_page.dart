13cd7b71ae21e72ca45722fa75f0dd9aa02e86c8
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-28 12:37:56 +0300

    fix

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -2,23 +2,6 @@ import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   final String _versionName = 'V1.0';
-  /*final splashDelay = 3;
-
-  _loadWidget() async {
-    var _duration = Duration(seconds: splashDelay);
-    return Timer(_duration, navigationPage);
-  }
-
-  void navigationPage() {
-    Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (BuildContext context) => HomePage()));
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadWidget();
-  }*/
 
   @override
   Widget build(BuildContext context) {
